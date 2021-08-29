144ebe3f2e60e043bb199b5486a7a1669f95cc36
stian34556 <77517664+stian34556@users.noreply.github.com>
2021-08-28 22:35:41 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCCKT9-A7YvTyRqstDX-KXup8WFeuuC36I",
+    authDomain: "login-prac-9f3f5.firebaseapp.com",
+    projectId: "login-prac-9f3f5",
+    storageBucket: "login-prac-9f3f5.appspot.com",
+    messagingSenderId: "434901184878",
+    appId: "1:434901184878:web:7df8f3e4ec3661ffb1117f",
+    measurementId: "G-79YH2NDSXR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
